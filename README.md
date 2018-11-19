848257da8ada371b099ecd27af991443dcacbb46
NeverL3gend <alejandrogarcicojulun@gmail.com>
2018-11-19 01:47:09 -0500

    Barista-App pushed

---
+++ README.md
@@ -0,0 +1,4 @@
+Usage:
+1.Run npm install
+2.run node server.js
+3.Navigate to localhost:8080 in your browser
---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+// exports an object with a link to database
+module.exports = {
+
+    'url' : 'mongodb://barista:A123456@ds163013.mlab.com:63013/barista2' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
